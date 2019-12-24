3de49256b11c5d97df1e671d01d506a0ef3e64b5
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-12-24 15:57:51 +0300

    Add base tests for calc

--- src/test/java/me/darkweird/testing/spring/ExampleTest.java
+++ src/test/java/me/darkweird/testing/spring/ExampleTest.java
@@ -1,5 +1,7 @@
 package me.darkweird.testing.spring;
 
+
+import me.darkweird.testing.service.CalculatorSoap;
 import me.darkweird.testing.service.ExampleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
@@ -12,8 +14,33 @@ public class ExampleTest extends SpringBaseTest {
     @Autowired
     ExampleService exampleService;
 
+    @Autowired
+    CalculatorSoap calculatorSoap;
+
     @Test
     public void exampleTest() {
         Assert.assertEquals(exampleService.returnsFour(), 4);
     }
+
+    @Test
+    public void addTest() {
+        Assert.assertEquals(calculatorSoap.add(2, 2), 4);
+    }
+
+    @Test
+    public void subtractTest() {
+        Assert.assertEquals(calculatorSoap.subtract(6, 2), 4);
+    }
+
+    @Test
+    public void multiplyTest() {
+        Assert.assertEquals(calculatorSoap.multiply(2, 2), 4);
+    }
+
+    @Test
+    public void divideTest() {
+        Assert.assertEquals(calculatorSoap.divide(2, 2), 1);
+    }
+
+
 }
